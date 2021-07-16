185aeaa0db7aa5e38e378aae40fd6d960b31da6c
Yinavis Segovia <yinavismassiel@hotmail.com>
2021-07-16 17:57:04 -0500

    Cambios en la clase MiClase para trabajar con github action y sonarcloud

--- src/main/java/MiClase.java
+++ src/main/java/MiClase.java
@@ -1,6 +1,14 @@
 public class MiClase {
     public static void main(String args[]) {
-        System.out.println("Este es un proyecto Java con Maven");
+        var a = 2;
+        if (a == 0) {
+            System.out.println("El valor de a es 0");
+        } else if (a == 1) {
+            System.out.println("El valor de a es 1");
+        } else if (a == 2){
+            System.out.println("El valor de a es 2");
+        } else{
+            System.out.println("El valor de a es desconocido");
+        }
     }
 }
-
